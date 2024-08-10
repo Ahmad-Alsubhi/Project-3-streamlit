91232420ddf8a441b63aa45f1138d5601c8ae7a9
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-10 13:57:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,34 +8,31 @@ url ='https://raw.githubusercontent.com/AbdullahSoli/Project-3-streamlit/main/cl
 #df= pd.read_csv('cleaned_RiyadhVillasAqar.csv')
 df= pd.read_csv(url)
 st.write('Hello world!')
-st.write('Hello world!gjgjgjg')
 
-st.image("riyadh.jpg", caption="Riyadh",width=400)
 st.html(
-    "<h1>Abdullah</h1>"
-    "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
+    "<h1>الفلل في الرياض </h1>"
+    "<p><span style='text-decoration: line-through double red;'>مقدمة</span>!</p>"
 )
 
-data = pd.DataFrame({
-    'labels': ['A', 'B', 'C', 'D'],
-    'sizes': [15, 30, 45, 10]
-})
+st.image("riyadh.jpg", caption="Riyadh",width=1000)
 
-# Create an Altair pie chart
-chart = alt.Chart(data).mark_arc().encode(
-    theta=alt.Theta(field='sizes', type='quantitative'),
-    color=alt.Color(field='labels', type='nominal')
-).properties(title='Pie Chart Example')
-
-# Display the chart in Streamlit
-st.altair_chart(chart, use_container_width=True)
-
+st.html(
+    "<h1>الفلل في الرياض </h1>"
+    "<p>مقدمة</p>"
+)
+st.html(
+    "<h1>السؤال الاول ؟ </h1>"
+    "<p>شرح</p>"
+)
 data = pd.DataFrame({
     'labels': ['Maid Room', 'Driver Room'],
     'sizes': [df['maidRoom'].sum(), df['driverRoom'].sum()]
 })
 
-# إنشاء مخطط دائري باستخدام Altair
+st.html(
+    "<h1>السؤال الثاني ؟ </h1>"
+    "<p>شرح</p>"
+)
 chart = alt.Chart(data).mark_arc().encode(
     theta=alt.Theta(field='sizes', type='quantitative'),
     color=alt.Color(field='labels', type='nominal'),
@@ -44,5 +41,5 @@ chart = alt.Chart(data).mark_arc().encode(
     title='Maid Room and Driver Room'
 )
 
-# عرض المخطط في Streamlit
+
 st.altair_chart(chart, use_container_width=True)
