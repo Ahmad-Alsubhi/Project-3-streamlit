8282e8794d15c3d43a555d5fef1b70880640e12b
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-10 12:06:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
 df= pd.read_csv('cleaned_RiyadhVillasAqar.csv')
 st.write('Hello world!')
 st.write('Hello world!gjgjgjg')
@@ -9,15 +13,17 @@ st.html(
     "<h1>Abdullah</h1>"
     "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
 )
-import streamlit as st
-import pandas as pd
-import numpy as np
-import altair as alt
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['location', 'price'])
 
-st.map(df)
+duplex_counts = df['duplex'].value_counts()
+
+# Create the pie chart
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(duplex_counts, labels=['Duplex', 'not Duplex'], autopct='%1.1f%%')
+ax.set_title('Distribution of Duplex Values')
+ax.axis('equal')  # Equal aspect ratio ensures that pie is circular.
+
+# Display the chart
+plt.show()
 
 
