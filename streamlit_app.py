f4438c43d117e3dc1922fcbab730079f914ecca9
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-10 13:03:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import plotly.figure_factory as ff
+#import plotly.figure_factory as ff
 #import matplotlib.pyplot as plt
 url ='https://raw.githubusercontent.com/AbdullahSoli/Project-3-streamlit/main/cleaned_RiyadhVillasAqar2.csv'
 #df= pd.read_csv('cleaned_RiyadhVillasAqar.csv')
