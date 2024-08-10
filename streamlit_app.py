cdd1aac92535c3ae998333166732341d646fc1f2
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-10 12:02:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+df= pd.read_csv('cleaned_RiyadhVillasAqar.csv')
 st.write('Hello world!')
 st.write('Hello world!gjgjgjg')
 
@@ -13,14 +14,10 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['location', 'price'])
 
-c = (
-   alt.Chart(chart_data)
-   .mark_circle()
-   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
-)
-
-st.altair_chart(c, use_container_width=True)
+st.map(df)
 
 
